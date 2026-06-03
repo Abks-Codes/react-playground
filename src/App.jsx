9cf82d2e1ec3205b8c68c612e5c3ad65bbd99b9f
Abikesh Shrestha <shresthaabks@gmail.com>
2026-06-02 21:37:06 -0700

    Set up React playground and first component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MovieCard from './MovieCard'
+
+function App() {
+  return (
+    <>
+    <h1>K7 Movie Finder</h1>
+    <p>It is a movie finder made by Kirito</p>
+    <MovieCard title="naruto" year="2002"/>
+    <MovieCard title="sasuke" year="2003"/>
+    <MovieCard title="sakura" year="2004"/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,6 @@
+function helloWorld(){
+    return(
+        <h1>Hello World</h1>
+        
+    )
+}
